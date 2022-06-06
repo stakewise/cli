998e63fe4277b9cb11a2280e999bfc3e99a6cd82
Alexander Sysoev <avsysoev60@gmail.com>
2022-06-06 22:28:32 +0300

    Add sync-db command (#37)
    * Add sync-db command
    
    * Sync-db: Review fixes
    
    * Sync-db: Remove public key warning
    
    * Sync-db: Add confirmation
    
    * Sync-db: Fix typo
    
    * Sync-db: Update keys iteration
    
    * Sync-db: process all keys
    
    * Sync-db: Use sets for keys
    
    * Update function description
    
    Co-authored-by: Dmitri Tsumak <tsumak.dmitri@gmail.com>

---
+++ stakewise_cli/commands/sync_db.py
@@ -0,0 +1,100 @@
+import re
+
+import click
+import psycopg2
+from eth_typing import ChecksumAddress
+from eth_utils import is_address, to_checksum_address
+
+from stakewise_cli.eth2 import validate_mnemonic
+from stakewise_cli.networks import (
+    GNOSIS_CHAIN,
+    GOERLI,
+    HARBOUR_GOERLI,
+    HARBOUR_MAINNET,
+    MAINNET,
+)
+from stakewise_cli.storages.database import Database, get_db_connection
+
+
+def validate_operator_address(ctx, param, value):
+    try:
+        if is_address(value):
+            return to_checksum_address(value)
+    except ValueError:
+        pass
+
+    raise click.BadParameter("Invalid Ethereum address")
+
+
+def validate_db_uri(ctx, param, value):
+    pattern = re.compile(r".+:\/\/.+:.*@.+\/.+")
+    if not pattern.match(value):
+        raise click.BadParameter("Invalid database connection string")
+    return value
+
+
+@click.command(help="Synchronizes validator keystores in the database for web3signer")
+@click.option(
+    "--network",
+    default=MAINNET,
+    help="The network you are targeting.",
+    prompt="Please choose the network name",
+    type=click.Choice(
+        [MAINNET, GOERLI, HARBOUR_MAINNET, HARBOUR_GOERLI, GNOSIS_CHAIN],
+        case_sensitive=False,
+    ),
+)
+@click.option(
+    "--operator",
+    help="The operator wallet address specified during deposit data generation.",
+    prompt="Enter your operator wallet address",
+    callback=validate_operator_address,
+)
+@click.option(
+    "--db-url",
+    help="The database connection address.",
+    prompt="Enter the database connection string, ex. 'postgresql://username:pass@hostname/dbname'",
+    callback=validate_db_uri,
+)
+def sync_db(network: str, operator: ChecksumAddress, db_url: str) -> None:
+
+    # check connection
+    connection = get_db_connection(db_url=db_url)
+    try:
+        cur = connection.cursor()
+        cur.execute("SELECT 1")
+    except psycopg2.OperationalError as e:
+        raise click.ClickException(
+            f"Error: failed to connect to the database server with provided URL. Error details: {e}",
+        )
+
+    mnemonic = click.prompt(
+        'Enter your mnemonic separated by spaces (" ")',
+        value_proc=validate_mnemonic,
+        type=click.STRING,
+        hide_input=True,
+    )
+
+    click.clear()
+    database = Database(
+        db_url=db_url,
+        operator=operator,
+        network=network,
+        mnemonic=mnemonic,
+    )
+
+    click.confirm(
+        f"Synced {len(database.keys)} key pairs, apply changes to the database?",
+        default=True,
+        abort=True,
+    )
+    database.apply_changes()
+
+    click.secho(
+        f"The database contains {len(database.keys)} validator keys.\n"
+        f'Please upgrade the "validators" helm chart with "validatorsCount" set to {database.validators_count}\n'
+        f'and "reimportKeystores" set to "true".\n'
+        f'Set "decryptionKey" to "{database.cipher_key_str}"',
+        bold=True,
+        fg="green",
+    )
--- stakewise_cli/main.py
+++ stakewise_cli/main.py
@@ -13,6 +13,7 @@ from stakewise_cli.commands.create_shard_pubkeys import (  # noqa: E402
 )
 from stakewise_cli.commands.get_mnemonic_pubkey import get_mnemonic_pubkey  # noqa: E402
 from stakewise_cli.commands.get_pubkey_index import get_pubkey_index  # noqa: E402
+from stakewise_cli.commands.sync_db import sync_db  # noqa: E402
 from stakewise_cli.commands.sync_local import sync_local  # noqa: E402
 from stakewise_cli.commands.sync_vault import sync_vault  # noqa: E402
 from stakewise_cli.commands.upload_deposit_data import upload_deposit_data  # noqa: E402
@@ -33,6 +34,7 @@ cli.add_command(upload_deposit_data)
 cli.add_command(verify_deposit_data)
 cli.add_command(sync_vault)
 cli.add_command(sync_local)
+cli.add_command(sync_db)
 cli.add_command(create_shard_pubkeys)
 cli.add_command(verify_shard_pubkeys)
 cli.add_command(get_mnemonic_pubkey)
