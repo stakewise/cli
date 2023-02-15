783f1153a88d53bf64aa693ce063f0d935f27c6e
concentricspheres <107502292+concentricspheres@users.noreply.github.com>
2023-02-14 19:48:18 -0600

    actually write the keys to the database

--- stakewise_cli/commands/sync_db.py
+++ stakewise_cli/commands/sync_db.py
@@ -1,5 +1,5 @@
 import os
-import json
+import math
 
 from typing import Dict
 
@@ -17,12 +17,6 @@ from stakewise_cli.web3signer import Web3SignerManager
 from staking_deposit.key_handling.keystore import Keystore
 
 @click.command(help="Synchronizes validator keystores in the database for web3signer")
-@click.option(
-    "--operator",
-    help="The operator wallet address specified during deposit data generation.",
-    prompt="Enter your operator wallet address",
-    callback=validate_operator_address,
-)
 @click.option(
     "--db-url",
     help="The database connection address.",
@@ -42,37 +36,25 @@ from staking_deposit.key_handling.keystore import Keystore
     type=click.Path(exists=False, file_okay=False, dir_okay=True),
 )
 def sync_db(
-    operator: ChecksumAddress,
     db_url: str,
     validator_capacity: int,
     private_keys_dir: str,
 ) -> None:
-    # check_db_connection(db_url)
+    check_db_connection(db_url)
 
-    # beacon_client = prompt_beacon_client(network)
-
-    # mnemonic = click.prompt(
-    #     'Enter your mnemonic separated by spaces (" ")',
-    #     value_proc=validate_mnemonic,
-    #     type=click.STRING,
-    # )
     decrypt_key = click.prompt(
         'Enter the password to decrypt validators private keys',
         type=click.STRING,
         hide_input=True
     )
-    # click.clear()
 
-    # web3signer = Web3SignerManager(
-    #     operator=operator,
-    #     network=MAINNET,
-    #     # mnemonic=mnemonic,
-    #     validator_capacity=validator_capacity,
-    #     # beacon=beacon_client,
-    # )
-    # database = Database(
-    #     db_url=db_url,
-    # )
+    web3signer = Web3SignerManager(
+        validator_capacity=validator_capacity,
+    )
+
+    database = Database(
+        db_url=db_url,
+    )
 
     keypairs: Dict[HexStr, int] = dict()
 
@@ -100,26 +82,17 @@ def sync_db(
         default=True,
         abort=True,
     )
-    
-    # keys = web3signer.keys
-    # if private_keys_dir and decrypt_key:
-    #     
-    #     transferred_keypairs = decrypt_transferred_keys(
-    #         keys_dir=private_keys_dir, decrypt_key=decrypt_key
-    #     )
-    #     keys.extend(web3signer.process_transferred_keypairs(transferred_keypairs))
-    #     click.confirm(
-    #         f"Synced {len(transferred_keypairs)} transferred key pairs, apply changes to the database?",
-    #         default=True,
-    #         abort=True,
-    #     )
         
-    # database.update_keys(keys=keys)
+    keys = web3signer.process_transferred_keypairs(keypairs)
+
+    validators_count = math.ceil(len(keypairs) / validator_capacity)
+
+    database.update_keys(keys=keys)
 
     click.secho(
         f"The database contains {len(keypairs)} validator keys.\n"
-        # f"Please upgrade the 'validators' helm chart with 'validatorsCount' set to {web3signer.validators_count}\n"
-        # f"Set 'DECRYPTION_KEY' env to '{private_key}'",
+        f"Please upgrade the 'validators' helm chart with 'validatorsCount' set to {validators_count}\n"
+        f"Set 'DECRYPTION_KEY' env to '{web3signer.encoder.cipher_key_str}'",
         bold=True,
         fg="green",
     )
